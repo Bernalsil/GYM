acd59e39dcdf19faba54e76c093fc3432bb0b82e
Yair <yairmasterlol@gmail.com>
2023-07-11 21:49:01 -0600

    2023-07-11-09:49:01

--- silgym/settings.py
+++ silgym/settings.py
@@ -79,10 +79,20 @@ WSGI_APPLICATION = "silgym.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "maQBBIpgWJE43zIsK9eG",
+        "HOST": "containers-us-west-97.railway.app",
+        "PORT": "6165",
     }
 }
 
