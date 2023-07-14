f77f552a75b26774d905b3a60806d22ac1b6cc4c
yair <yairmasterlol@gmail.com>
2023-07-13 23:23:06 -0600

    2023-07-13-11:23:06 PM

--- silgym/settings.py
+++ silgym/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "project/build/static")]
+STATIC_FILES = ((os.path.join(BASE_DIR, "silgym/static")),)
 
 
 # Default primary key field type
