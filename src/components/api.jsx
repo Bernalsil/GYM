acf0496e50fac97f13f8803ee676cbb273df669f
yair <yairmasterlol@gmail.com>
2023-07-15 22:42:59 -0600

    2023-07-15-10:42:59 PM

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,2 +1,4 @@
-export const api = "http://127.0.0.1:8000/api";
-export const server = "http://127.0.0.1:8000";
+export const api = "https://gym-production-dec8.up.railway.app/api";
+export const server = "https://gym-production-dec8.up.railway.app";
+// export const api = "http://127.0.0.1:8000/api";
+// export const server = "http://127.0.0.1:8000";
